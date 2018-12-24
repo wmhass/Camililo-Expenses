25dadbf8bae02ad88e1b9249990a46fe9d6a6d51
William Hass <wm.hass@gmail.com>
2018-12-23 18:27:28 -0800

    Initial Commit

---
+++ Camililo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Camililo
+//
+//  Created by William Hass on 2018-12-23.
+//  Copyright © 2018 lilohass. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
